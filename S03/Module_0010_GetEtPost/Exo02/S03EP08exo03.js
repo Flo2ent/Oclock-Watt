bcc40daa3ad03a7a46265c18a353e500c1ed81b7
Flo2ent <flo2ent@gmail.com>
2023-05-05 14:06:41 +0200

    CSS en plus

---
+++ S03/Module_0010_GetEtPost/Exo02/S03EP08exo03.js
@@ -0,0 +1,57 @@
+const express = require("express");
+
+const errorHandler500 = require("./middlewares/errorHandler500");
+const pageNotFound404 = require("./middlewares/pageNotFound404");
+
+const app = express();
+
+let guests = [];
+
+let guestId = 1;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+// Permet de recuperer les valeurs des requetes POST
+app.use(express.urlencoded({ extended: true }));
+
+// Permet de recuperer les valeurs JSON des requetes POST
+// app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index", { manageCSS: false });
+});
+
+// GET : permet de parametrer la route vers la page login
+app.get("/manage", (req, res) => {
+  res.render("manage", { guests, manageCSS: true });
+});
+
+// POST
+app.post("/manage", (req, res) => {
+  const person = req.body.firstName;
+  const id = req.body.removePersonButton;
+  if (person != "") {
+    guests.push({
+      firstName: person,
+      id: guestId++,
+    });
+  }
+  if (id != null) {
+    guests.find;
+    //Recuperation de l'objet x (la personne) qui a comme ID celui passé en paramètre
+    const personToRemove = guests.findIndex((x) => x.id == id);
+    console.log(personToRemove);
+    // Supprimer la personne de la liste des invites ayant l'id
+    guests.splice(personToRemove, 1);
+  }
+  res.render("manage", { guests, manageCSS: true });
+});
+
+// Error handling middleware
+app.use(errorHandler500);
+
+app.use(pageNotFound404);
+
+app.listen(3000);
--- S03/Module_0010_GetEtPost/Exo02/modules/randomNumberMinMax.js
+++
@@ -1,5 +0,0 @@
-function randomNumberMinMax(min, max) {
-  return Math.floor(Math.random() * (max - min) + min + 1);
-}
-
-module.exports = randomNumberMinMax;
